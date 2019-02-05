2b600453815e6b7e1e626b4777250863c995f08f
nattrigueros <nattrigueros00@gmail.com>
2019-02-05 15:32:03 +0100

    This is Activity 4 :)

---
+++ session-5/ex-1-user.py
@@ -0,0 +1,21 @@
+def count_a(seq):
+
+    result = 0
+    for b in seq:
+        if b == 'A':
+            result += 1
+
+    return result
+
+
+# Main program
+
+s = str(input('Please, enter a valid sequence: '))
+na = count_a(s)
+print("The are {} As in the sequence".format(na))
+
+# Calculate the total length
+tl = len(s)
+
+print("This sequence is {} bases in length".format(tl))
+print("The percentages of As is {}%".format(round(100.0 * na/tl, 1)))
