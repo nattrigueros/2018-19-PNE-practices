0c721008e38141e9c7778799c2edaa183ada1175
nattrigueros <nattrigueros00@gmail.com>
2019-02-05 15:49:02 +0100

    This is Activity

--- session-5/ex-1-user.py
+++ session-5/ex-1-user.py
@@ -10,12 +10,17 @@ def count_a(seq):
 
 # Main program
 
-s = str(input('Please, enter a valid sequence: '))
+s = (input('Please, enter a valid sequence: '))
 na = count_a(s)
 print("The are {} As in the sequence".format(na))
 
 # Calculate the total length
 tl = len(s)
 
+if tl>0:
+    perc=round(100.0*n/tl,1)
+else:
+    perc=0
+
 print("This sequence is {} bases in length".format(tl))
-print("The percentages of As is {}%".format(round(100.0 * na/tl, 1)))
+print("The percentages of As is {}%".format(perc))
