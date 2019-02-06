b6db58c18cc7e0b25f2baca712ce46292082732c
nattrigueros <nattrigueros00@gmail.com>
2019-02-06 16:59:09 +0100

    This is bases.py

--- session-5/bases.py
+++ session-5/bases.py
@@ -1,2 +1,18 @@
-from exercise-1 import count_bases
-count_bases()
+def count_bases(seq):
+
+    counter_a = 0
+    counter_t = 0
+    counter_g = 0
+    counter_c = 0
+
+    for letter in seq:
+        if letter == 'A':
+            counter_a += 1
+        if letter == 'T':    # In this loop we count the number of instances each base appears in the sequence
+            counter_t += 1
+        if letter == 'G':
+            counter_g += 1
+        if letter == 'C':
+            counter_c += 1
+
+    return counter_a, counter_t, counter_g, counter_c
