173528245b2b8b7fac5cb4eaa5c9fbb27a0103e4
nattrigueros <nattrigueros00@gmail.com>
2019-03-22 12:12:07 +0100

    This is EXERCISE 1 :)

---
+++ session-17/Exercise1.py
@@ -0,0 +1,38 @@
+import json
+import termcolor
+
+# -- Open the json file
+f = open("person_ex1.json", 'r')
+
+# Read the data from the file
+# Now person is a dictionary with all the information
+persons = json.load(f)
+
+print('Total people:', len(persons['Persons']))
+
+list_people = persons['Persons']
+for person in list_people:  # CODE FOR PRINTING THE NAME ,LAST NAME, PHONE NUMBER, AND AGE OF THE PATIENTS BY INDEXING
+    # Print the information in the object
+    print()
+    termcolor.cprint("Name: ", 'green', end="")
+    print(person['Firstname'], person['Lastname'])
+
+    termcolor.cprint("Age: ", 'green', end="")
+    print(person['age'])
+
+    # Get the phoneNumber list
+    phoneNumbers = person['phoneNumber']
+
+    # Print the number of elements int the list
+    termcolor.cprint("Phone numbers: ", 'green', end='')
+    print(len(phoneNumbers))
+
+    # Print all the numbers
+    for i, num in enumerate(phoneNumbers):  # FOT THE PHONE NUMBERS WE NEED TO INDEX AGAIN
+        termcolor.cprint("  Phone {}:".format(i), 'blue')
+
+        # The element num contains 2 fields: number and type
+        termcolor.cprint("    Type: ", 'red', end='')
+        print(num['type'])
+        termcolor.cprint("    Number: ", 'red', end='')
+        print(num['number'])
